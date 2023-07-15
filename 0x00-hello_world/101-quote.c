532f201716cf78c62c02ce3829d1b07ef64606bf
Derrick <derrickflynt@gmail.com>
2023-07-15 06:56:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <uninsted.h>
+#include <unistd.h>
 /**
- * main - Entry point;
+ * main - Entry point
  *
  * Return: Always 1 always(Error)
  */
-int main(void);
 
-{ 
+int main(void)
+{
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1); 
-
-}	
+	return (1);
+}
