103eef68eed4778b224100d4f4892d4501d7869a
Derrick <derrickflynt@gmail.com>
2023-07-15 04:33:01 -0700

    byte sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+ 
 	char c;
 	int i;
-	long i;
-	long long i;
+	long l;
+	long long ll;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+        printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-	printf("Size of a char: %lu byte(s)\n", sizeof(f));
-	
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
 	return (0);
+
 }
