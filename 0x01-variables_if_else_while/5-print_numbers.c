611182c76fec127b7e53594e6e5c8438782ebcce
Derrick <derrickflynt@gmail.com>
2023-07-16 19:20:14 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - numbers in base 10
+ *
+ * Return: 0 always
+ *
+ */
+int main(void)
+{
+	int character;
+
+	for (character = 0 ; character < 10 ; character++)
+		printf("%d", character);
+	return (0);
+}
