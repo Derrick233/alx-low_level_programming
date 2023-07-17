0153e78030e3b32079803ce0b8137395c446a9c8
Derrick <derrickflynt@gmail.com>
2023-07-17 07:20:14 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,16 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 0 ; digit < 10 ; digit++)
+	for (digit = 0; digit <= 9; digit++)
 	{
 		putchar((digit % 10) + '0');
-
 		if (digit == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
