267880e0e256507fa057affd6c551f0a8a218244
Derrick <derrickflynt@gmail.com>
2023-07-21 10:09:21 -0700

    nested loops

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (a > 0)
+		printf("%d is positive\n", a);
+	else if (a == 0 )
+		printf ("%d is zero\n", a);
+	else
+		printf("%d is negative\n", a);
+}
